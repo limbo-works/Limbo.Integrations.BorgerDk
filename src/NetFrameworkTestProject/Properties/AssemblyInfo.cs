29d05828462dc2c8534db5fe11787d3af7e2d814
Anders Bjerner <abjerner@skybrud.dk>
2024-01-09 21:07:21 +0100

    Added unit tests projects for .NET Framework 4.7.2 and .NET 8

---
+++ src/NetFrameworkTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetFrameworkTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetFrameworkTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0f6b0c1-becc-4eeb-8838-c231c8430524")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/NetFrameworkTestProject/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Limbo.Integrations.BorgerDk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetFrameworkTestProject {
+
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void GetArticleList1() {
+
+            // Initialize a new HTTP service
+            BorgerDkHttpService http = new BorgerDkHttpService(BorgerDkEndpoint.Default);
+
+            // Get the article list
+            IReadOnlyList<BorgerDkArticleDescription> articles = http.GetArticleList();
+
+            // Should have more than 300 articles
+            Assert.IsTrue(articles.Count > 300);
+
+        }
+
+        [TestMethod]
+        public void GetArticleList2() {
+
+            // Initialize a new HTTP service
+            BorgerDkHttpService http = new BorgerDkHttpService(BorgerDkEndpoint.LifeInDenmark);
+
+            // Get the article list
+            IReadOnlyList<BorgerDkArticleDescription> articles = http.GetArticleList();
+
+            // Should have more than 150 articles
+            Assert.IsTrue(articles.Count > 150);
+
+        }
+
+        [TestMethod]
+        public void GetById() {
+
+            // Initialize a new HTTP service
+            BorgerDkHttpService http = new BorgerDkHttpService(BorgerDkEndpoint.Default);
+
+            // Get the article with ID "11203"
+            BorgerDkArticle article = http.GetArticleFromId(11203, BorgerDkMunicipality.VejleKommune);
+
+            // Validate the article
+            Assert.AreEqual(11203, article.Id);
+            Assert.AreEqual("Skattefri præmie", article.Title);
+            Assert.AreEqual("Hvis du arbejder i efterlønsperioden, kan du eventuelt opnå ret til en skattefri præmie", article.Header);
+            Assert.AreEqual("Skrevet af Styrelsen for Arbejdsmarked og Rekruttering", article.ByLine);
+            Assert.AreEqual("https://www.borger.dk/pension-og-efterloen/Efterloen-fleksydelse-delpension/efterloen/skattefri-praemie", article.Url);
+
+        }
+
+        [TestMethod]
+        public void GetByUrl() {
+
+            // Initialize a new HTTP service
+            BorgerDkHttpService http = new BorgerDkHttpService(BorgerDkEndpoint.Default);
+
+            // Get the article with ID "11203"
+            BorgerDkArticleShortDescription article = http.GetArticleIdFromUrl("https://www.borger.dk/pension-og-efterloen/Efterloen-fleksydelse-delpension/efterloen/skattefri-praemie");
+
+            // Validate the article item
+            Assert.AreEqual(11203, article.Id);
+            Assert.AreEqual("Skattefri præmie", article.Title);
+
+        }
+
+    }
+
+}
